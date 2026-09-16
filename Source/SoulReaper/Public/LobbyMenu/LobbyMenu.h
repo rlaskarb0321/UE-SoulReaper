7faab631c6c19973f287fc06e59a6e3da090ac0b
Kkabydol <rlaskarb0321@gmail.com>
2026-09-16 15:20:57 +0900

    [lobby level] create lobby menu wbp

---
+++ Source/SoulReaper/Public/LobbyMenu/LobbyMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULREAPER_API ULobbyMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
